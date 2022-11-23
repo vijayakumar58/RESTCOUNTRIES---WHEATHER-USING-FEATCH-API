d23cab4669e9dc5377eb1ee1efe8bd8c2a9341b9
vijayakumar58 <vijayalingam58@gmail.com>
2022-11-23 16:34:26 +0530

    add js

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ async function getweatherData(){
      console.log(countrieData);
  
      const cityname=countrieData[0].name.common
+     console.log(cityname);
      const flag=countrieData[0].flags.png
      console.log(flag);
      let flags=document.getElementsByClassName("card-img");
-     
+        flags.item=flags
      let weatherApi=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=87fe926676d0f2ab9b9cda9b1587fb87`)
      let weatherData=await weatherApi.json();
      console.log(weatherData);
-}
+     let temperature=weatherData.main.temp
+     console.log(temperature)
+     let pressure=weatherData.main.pressure
+     console.log(pressure)
+     let windSpeed=weatherData.wind.speed
+     console.log(windSpeed);
+};
 getweatherData();
