149764b04375df05b0267739a655d1e86e1a9083
vijayakumar58 <vijayalingam58@gmail.com>
2022-11-25 13:17:15 +0530

    add js

--- script.js
+++ script.js
@@ -17,40 +17,44 @@ async function getweatherData(){
          divrow.innerHTML+=`<div class="col-lg-4">
          <div class="col-sm-12"></div>
          <div class="card">
-           <div class="card-header">${ele.name.common}</div>
+           <div class="card-header" id="countryName"><b>${ele.name.common}</b></div>
            <div class="card-body">
-               <img src=${ele.flags.png}class="card-img" alt="national flag">
-                   <div class="text">Capital :${ele.capital}</div>
-                   <div class="text">Region :${ele.name.region}</div>
-                   <div class="text">Country Code :${ele.cca2}</div>
+              <div class="divimage"> <img src= "${ele.flags.png}" class="card-img" alt="national flag"> </div>
+                   <div class="text"><b>Capital :${ele.capital}</b></div>
+                   <div class="text"><b>Region :${ele.region}</b></div>
+                   <div class="text"><b>Country Code :${ele.cca2}</b></div>
+              <div class="wheatherDetails"></div>     
            </div>
-           <button class="btn btn-primary">Click for weather</button>
+           <button class="btn btn-primary" onclick="countryWheatherData()">Click for weather</button>
          </div>
        </div>`
-      //  let weatherApi=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ele.name.common}&appid=87fe926676d0f2ab9b9cda9b1587fb87`)
-      //  let weatherData=await weatherApi.json();
-      //  console.log(weatherData);
-      //  let temperature=weatherData.main.temp
-      //  console.log(temperature)
-      //  let pressure=weatherData.main.pressure
-      //  console.log(pressure)
-      //  let windSpeed=weatherData.wind.speed
-      //  console.log(windSpeed);
-      });
-     const cityname=countrieData[0].name.common
-     console.log(cityname);
-     const flag=countrieData[0].flags.png
-     console.log(flag);
-     let flags=document.getElementsByClassName("card-img");
-        flags.item=flags
-     let weatherApi=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=87fe926676d0f2ab9b9cda9b1587fb87`)
-     let weatherData=await weatherApi.json();
-     console.log(weatherData);
-     let temperature=weatherData.main.temp
-     console.log(temperature)
-     let pressure=weatherData.main.pressure
-     console.log(pressure)
-     let windSpeed=weatherData.wind.speed
-     console.log(windSpeed);
+      //  async function countryWheatherData(){
+      //   let wheatherCountry=document.getElementById("countryName").value;
+      //   console.log(wheatherCountry);
+      //   let result=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${wheatherCountry}&appid=87fe926676d0f2ab9b9cda9b1587fb87`)
+      //   let res=await result.json();
+      //   console.log(res);
+      //  }
+      //  countryWheatherData();
+       });
+       let wheatherCountry=document.getElementById("countryName");
+       async function countryWheatherData(){
+          console.log(wheatherCountry);
+          let result=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${wheatherCountry}&appid=87fe926676d0f2ab9b9cda9b1587fb87`)
+          let res=await result.json();
+          console.log(res);
+         }
+        countryWheatherData();
+    //  const cityname=countrieData[0].name.common
+    //  console.log(cityname);
+    //  let weatherApi=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=87fe926676d0f2ab9b9cda9b1587fb87`)
+    //  let weatherData=await weatherApi.json();
+    //  console.log(weatherData);
+    //  let temperature=weatherData.main.temp
+    //  console.log(temperature)
+    //  let pressure=weatherData.main.pressure
+    //  console.log(pressure)
+    //  let windSpeed=weatherData.wind.speed
+    //  console.log(windSpeed);
 };
 getweatherData();
