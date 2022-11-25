75d7fbbf98f23854c6495a38ddfb93176889f737
vijayakumar58 <vijayalingam58@gmail.com>
2022-11-25 18:02:48 +0530

    add js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const divcontainer=document.createElement("div");
  const divrow=document.createElement("div");
  divrow.setAttribute("class","row");
 
-// divcontainer.append(divrow);
+ divcontainer.append(divrow);
+ document.body.append(divcontainer);
 
 
 async function getweatherData(){
@@ -13,12 +14,8 @@ async function getweatherData(){
      console.log(countrieData);
   
       countrieData.map((ele)=>{
-         newcontainer=document.createElement("div")
-         newcontainer.setAttribute("class","row");
-         // console.log(ele.name.common);
          var country=ele.name.common;
-         // console.log(country);
-         newcontainer.innerHTML=`<div class="col-lg-4">
+         divrow.innerHTML+=`<div class="col-lg-4">
          <div class="col-sm-12"></div>
          <div class="card">
            <div class="card-header" id="countryName"><b>${ele.name.common}</b></div>
@@ -27,32 +24,23 @@ async function getweatherData(){
                    <div class="text"><b>Capital :${ele.capital}</b></div>
                    <div class="text"><b>Region :${ele.region}</b></div>
                    <div class="text"><b>Country Code :${ele.cca2}</b></div>
-              <div class="wheatherDetails"></div>     
+              <div class="wheatherDetails" id="details"></div>     
            </div>
-           <button class="btn btn-primary"onclick="weatherData(${country})">Click for weather</button>
+           <button class="btn btn-primary"onclick="weatherData('${country}')">Click for weather</button>
          </div>
        </div>`
-      divcontainer.append(newcontainer);
-      document.body.append(divcontainer);
        });
-       let wheatherCountry=document.getElementById("countryName");
-       async function weatherData(wheatherCountry){
-          console.log(wheatherCountry);
-          let result=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${wheatherCountry}&appid=87fe926676d0f2ab9b9cda9b1587fb87`)
-          let res=await result.json();
-          console.log(res);
-         }
-      //   countryWheatherData("india");
-    //  const cityname=countrieData[0].name.common
-    //  console.log(cityname);
-    //  let weatherApi=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=87fe926676d0f2ab9b9cda9b1587fb87`)
-    //  let weatherData=await weatherApi.json();
-    //  console.log(weatherData);
-    //  let temperature=weatherData.main.temp
-    //  console.log(temperature)
-    //  let pressure=weatherData.main.pressure
-    //  console.log(pressure)
-    //  let windSpeed=weatherData.wind.speed
-    //  console.log(windSpeed);
 };
 getweatherData();
+async function weatherData(Country){
+   console.log(Country);
+   let result=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Country}&appid=87fe926676d0f2ab9b9cda9b1587fb87`)
+   let res=await result.json();
+   console.log(res);
+  document.getElementById("details").innerHTML=`<span><b>Temperature : ${res.main.temp}</b></span>,<br>
+                                                <span><b>Pressure : ${res.main.pressure}</b></span>,<br>
+                                                <span><b>Humidity : ${res.main.humidity}</b></span>,<br>
+                                                <span><b>Wind Speed : ${res.wind.speed}</b></span>`
+  }
+  
+     
