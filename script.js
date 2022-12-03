aa1c4139818f712f66382967fe51e9cf654db6db
vijayakumar58 <vijayalingam58@gmail.com>
2022-12-03 13:25:27 +0530

    add js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function getweatherData(){
      let countrieData=await restCountries.json();
      console.log(countrieData);
   
-      countrieData.map((ele)=>{
+      countrieData.map((ele,index)=>{
          var country=ele.name.common;
          divrow.innerHTML+=`<div class="col-lg-4">
          <div class="card">
@@ -23,20 +23,24 @@ async function getweatherData(){
                    <div class="text"><b>Capital :${ele.capital}</b></div>
                    <div class="text"><b>Region :${ele.region}</b></div>
                    <div class="text"><b>Country Code :${ele.cca2}</b></div>
-              <div class="wheatherDetails" id="details"></div>     
+              <div class="wheatherDetails" style="display:none" id=details${index}></div>     
            </div>
-           <button class="btn btn-primary"onclick="weatherData('${country}')">Click for weather</button>
+           <button  type="button" class="btn btn-primary" value="${ele.name.common}" onclick="weatherData('${country}',${index})">Click for weather</button>
          </div>
        </div>`
        });
 };
 getweatherData();
-async function weatherData(Country){
+async function weatherData(Country,index){
+   console.log(index);
+   let details=document.getElementById('details'+index)
+   console.log(details);
    console.log(Country);
    let result=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Country}&appid=87fe926676d0f2ab9b9cda9b1587fb87`)
    let res=await result.json();
    console.log(res);
-  document.getElementById("details").innerHTML=`<span><b>Temperature : ${res.main.temp}</b></span>,<br>
+  details.style.display="block";
+  details.innerHTML=`<span><b>Temperature : ${res.main.temp}</b></span>,<br>
                                                 <span><b>Pressure : ${res.main.pressure}</b></span>,<br>
                                                 <span><b>Humidity : ${res.main.humidity}</b></span>,<br>
                                                 <span><b>Wind Speed : ${res.wind.speed}</b></span>`
